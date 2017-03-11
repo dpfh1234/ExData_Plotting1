b9c6254150319d0af84c04f304d70ae611d4b316
dpfh1234 <hathaway.danny@gmail.com>
2017-03-11 12:51:45 -0600

    Update Plot3.R
    Formatting

--- Plot3.R
+++ Plot3.R
@@ -10,30 +10,29 @@
                               "%d/%m/%Y %H:%M:%S") 
 
 # Create the Plot
-  sub.df$Globalactivepower <- as.numeric(sub.df$Global_active_power)
   sub.df$sub.metering.1 <- as.numeric(sub.df$Sub_metering_1)
   sub.df$sub.metering.2 <- as.numeric(sub.df$Sub_metering_2)
   sub.df$sub.metering.3 <- as.numeric(sub.df$Sub_metering_3)
   
   png("Plot3.png", width=480, height=480)
 
-  plot(sub.df$datetime, 
-       sub.df$sub.metering.1,
-       type = "l",
-       ylab = "Energy Submetering", 
-       xlab = "")
-  lines(sub.df$datetime, 
-        sub.df$sub.metering.2, 
+  plot(x     = sub.df$datetime, 
+       y     = sub.df$sub.metering.1,
+       type  = "l",
+       ylab  = "Energy Submetering", 
+       xlab  = "")
+  lines(x    = sub.df$datetime, 
+        y    = sub.df$sub.metering.2, 
         type = "l", 
         col  = "red")
-  lines(sub.df$datetime, 
-        sub.df$sub.metering.3, 
+  lines(x    = sub.df$datetime, 
+        y    = sub.df$sub.metering.3, 
         type = "l", 
         col  = "blue")
-  legend("topright", 
-         c("Sub_metering_1", 
-           "Sub_metering_2", 
-           "Sub_metering_3"), 
+  legend(x   = "topright", 
+         y   = c("Sub_metering_1", 
+                 "Sub_metering_2", 
+                 "Sub_metering_3"), 
          lty = 1, 
          lwd = 2, 
          col = c("black", "red", "blue"))
